431ab81b90bc4aa9ac8c380c79328f610847e0e5
Keith Baker <github@baker.af>
2022-07-19 23:12:41 -0400

    break up into files

--- Token.sol
+++ Token.sol
@@ -2,16 +2,15 @@
 
 pragma solidity ^0.8.10;
 
-// In Remix you can also directly import contracts from github repositories
-import "./Owned.sol";
+import "./Ownable.sol";
 
-contract InheritanceModifierExample is Owned {
-    mapping(address => uint256) public tokenBalance;
+contract Token is Owned {
 
-    uint256 tokenPrice = 1 ether;
+    mapping(address => uint) public tokenBalance;
+
+    uint tokenPrice = 1 ether;
 
     constructor() {
-        owner = msg.sender;
         tokenBalance[owner] = 100;
     }
 
@@ -20,23 +19,21 @@ contract InheritanceModifierExample is Owned {
     }
 
     function burnToken() public onlyOwner {
-        tokenBalance[owner]--; // we aren't checking about rolling over 0 here
+        tokenBalance[owner]--;
     }
 
     function purchaseToken() public payable {
-        require(
-            (tokenBalance[owner] * tokenPrice) / msg.value > 0,
-            "not enough tokens"
-        );
+        require((tokenBalance[owner] * tokenPrice) / msg.value > 0, "not enough tokens");
         tokenBalance[owner] -= msg.value / tokenPrice;
         tokenBalance[msg.sender] += msg.value / tokenPrice;
     }
 
-    function sendToken(address _to, uint256 _amount) public {
+    function sendToken(address _to, uint _amount) public {
         require(tokenBalance[msg.sender] >= _amount, "Not enough tokens");
         assert(tokenBalance[_to] + _amount >= tokenBalance[_to]);
         assert(tokenBalance[msg.sender] - _amount <= tokenBalance[msg.sender]);
         tokenBalance[msg.sender] -= _amount;
         tokenBalance[_to] += _amount;
     }
+
 }
