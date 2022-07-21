05a4cea0b78737c73ef92bb49f97c3b8abcb03fc
Keith Baker <github@baker.af>
2022-07-21 15:19:00 -0400

    Debugger example

---
+++ DebuggerExample.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+contract DebuggerExample {
+    uint256 public myUint;
+
+    function setMyUint(uint256 _myuint) public {
+        myUint = _myuint;
+    }
+}
