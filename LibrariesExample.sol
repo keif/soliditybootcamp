56ad22a3aaf7b8b481c8d68f4fb9444751ff96d8
Keith Baker <github@baker.af>
2022-07-21 21:09:55 -0400

    Library with import

--- LibrariesExample.sol
+++ LibrariesExample.sol
@@ -2,7 +2,11 @@
 
 pragma solidity ^0.8.15;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
+
 contract LibrariesExample {
+    using SafeMath for uint256;
+
     mapping(address => uint256) public tokenBalance;
 
     constructor() {
@@ -10,8 +14,9 @@ contract LibrariesExample {
     }
 
     function sendToken(address _to, uint256 _amount) public returns (bool) {
-        tokenBalance[msg.sender] -= _amount;
-        tokenBalance[_to] += _amount;
+        tokenBalance[msg.sender] = tokenBalance[msg.sender].sub(_amount);
+        tokenBalance[_to] = tokenBalance[_to].add(_amount);
+
         return true;
     }
 }
